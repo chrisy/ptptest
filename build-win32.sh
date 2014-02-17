ab304248225267a0500f5bb094c232b6ab6b8968
Chris Luke <chris_luke@cable.comcast.com>
2014-02-16 23:32:06 -0500

    Update win32 builder

--- build-win32.sh
+++ build-win32.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+set -e
 rm -rf build
 rm -f ptptest-win32.zip
 /c/Python27-win32/python.exe build-binary.py build
 cd build
 mv exe.win32-2.7 ptptest-win32
-/c/cygwin/bin/zip -r ../ptptest-win32.zip ptptest-win32
+cp ../win32/*.exe ptptest-win32
+/c/cygwin/bin/zip -9 -r ../ptptest-win32.zip ptptest-win32
